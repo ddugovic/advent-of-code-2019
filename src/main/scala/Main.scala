fbbea37691cf50994f0347208bdc8fb95dcbebe8
Daniel Dugovic <dandydand@gmail.com>
2019-12-18 06:17:25 -0600

    Solve https://adventofcode.com/2019/day/7

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,4 +8,5 @@ object Main extends App {
   println("Problem04: " + new Problem04().run(Source.fromResource("04.txt").mkString.trim))
   println("Problem05: " + new Problem05().run(Source.fromResource("05.txt").mkString.trim))
   println("Problem06: " + new Problem06().run(Source.fromResource("06.txt").getLines.toList))
+  println("Problem07: " + new Problem07().run(Source.fromResource("07.txt").mkString.trim))
 }
---
+++ src/main/scala/Problem07.scala
@@ -0,0 +1,76 @@
+case class Problem07() {
+  class Computer(val memory: Array[Int], var pc: Int = 0, var output: Int = 0) {
+    def op = memory(pc) % 100
+    def read(mode: Int, address: Int) = if (mode % 10 == 1) memory(address) else memory(memory(address))
+    def write(address: Int, value: Int) = {memory(memory(address)) = value}
+    def step(input: Int) = {
+      val mode = memory(pc) / 100
+      op match {
+        case 1 => {
+          write(pc+3, read(mode, pc+1) + read(mode/10, pc+2))
+          pc += 4
+        }
+        case 2 => {
+          write(pc+3, read(mode, pc+1) * read(mode/10, pc+2))
+          pc += 4
+        }
+        case 3 => {
+          write(pc+1, input)
+          pc += 2
+        }
+        case 4 => {
+          output = read(mode, pc+1)
+          pc += 2
+        }
+        case 5 => {
+          pc = if (read(mode, pc+1) != 0) read(mode/10, pc+2) else pc + 3
+        }
+        case 6 => {
+          pc = if (read(mode, pc+1) == 0) read(mode/10, pc+2) else pc + 3
+        }
+        case 7 => {
+          write(pc+3, if (read(mode, pc+1) < read(mode/10, pc+2)) 1 else 0)
+          pc += 4
+        }
+        case 8 => {
+          write(pc+3, if (read(mode, pc+1) == read(mode/10, pc+2)) 1 else 0)
+          pc += 4
+        }
+        case 99 => pc 
+      }
+    }
+    def load(input: Int) = {
+      // ASSUME first instruction is an input
+      step(input)
+      this
+    }
+    def execute(input: Int) = {
+      var address = pc
+      while (op != 3 && step(input) != address) {
+        address = pc
+      }
+      if (op == 3) {
+        step(input)
+        while (op != 3 && step(input) != address) {
+          address = pc
+        }
+      }
+      this
+    }
+  }
+  class Network(memory: Array[Int], input: Seq[Int]) {
+    val computers = input.map(new Computer(memory.clone).load(_)).toList
+    def execute(input: Int) = computers.foldLeft(input)((i: Int, c: Computer) => c.execute(i).output)
+    def execute2(input: Int, iterations: Int): Int = {
+      var signal = input
+      1 to iterations foreach { _ => signal = execute(signal) }
+      signal
+    }
+  }
+  def run(inputs: String): String = {
+    val disk = inputs.split(",").map(_.toInt)
+    val phases = (0 to 4).permutations
+    val phases2 = (5 to 9).permutations
+    List(phases.map(new Network(disk, _).execute(0)).max, phases2.map(new Network(disk, _).execute2(0, 100)).max).mkString(" ")
+  }
+}
