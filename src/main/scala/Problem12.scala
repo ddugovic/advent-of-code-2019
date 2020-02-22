6164a84fc66cc1467cbb8de7a34627e7eac3f7c4
Daniel Dugovic <dandydand@gmail.com>
2020-02-22 09:26:19 -0600

    Convert Moon to an immutable case class

--- src/main/scala/Problem12.scala
+++ src/main/scala/Problem12.scala
@@ -5,29 +5,35 @@ case class Problem12() {
   case class Position(val x: Int, val y: Int, val z: Int) {
     def sum = x.abs + y.abs + z.abs
     def add(velocity: Velocity) = Position(x + velocity.dx, y + velocity.dy, z + velocity.dz)
+    def subtract(position: Position) = Position(x - position.x, y - position.y, z - position.z)
+    def signum: Tuple3[Int, Int, Int] = (x.signum, y.signum, z.signum)
   }
   case class Velocity(val dx: Int, val dy: Int, val dz: Int) {
     def sum = dx.abs + dy.abs + dz.abs
-    def add(ddx: Int, ddy: Int, ddz: Int) = Velocity(dx + ddx, dy + ddy, dz + ddz)
+    def add(ddp: Tuple3[Int, Int, Int]) = Velocity(dx + ddp._1, dy + ddp._2, dz + ddp._3)
   }
-  class Moon(val input: String) {
-    val m: Match = parser.findPrefixMatchOf(input).get
-    var position = Position(m.group(1).toInt, m.group(2).toInt, m.group(3).toInt)
-    var velocity = Velocity(0, 0, 0)
+  case class Moon(val position: Position, val velocity: Velocity) {
     def energy = position.sum * velocity.sum
-    def accelerate(moons: Seq[Moon]) = for (moon <- moons) {
-      this.velocity = this.velocity.add((moon.position.x - position.x).signum, (moon.position.y - position.y).signum, (moon.position.z - position.z).signum)
-    }
+    def accelerate(moons: Seq[Moon]): Velocity = moons.foldLeft(velocity)((velocity, moon) =>
+        velocity.add(moon.position.subtract(position).signum)
+    )
   }
-  def step(moons: Seq[Moon], steps: Int = 1) {
-    1 to steps foreach {_ =>
-      for (moon <- moons) moon.accelerate(moons)
-      for (moon <- moons) moon.position = moon.position.add(moon.velocity)
-    }
+  def newPosition(input: String): Position = {
+    val m: Match = parser.findPrefixMatchOf(input).get
+    Position(m.group(1).toInt, m.group(2).toInt, m.group(3).toInt)
+  }
+  def newMoon(input: String): Moon = Moon(newPosition(input), Velocity(0, 0, 0))
+  def step(moons: Seq[Moon]) = {
+    var newMoons = moons
+    val map = newMoons.zip(newMoons.map(_.accelerate(newMoons)))
+    map.map((tuple) => Moon(tuple._1.position.add(tuple._2), tuple._2))
   }
   def run(inputs: Seq[String]): String = {
-    val moons = inputs.map(new Moon(_))
-    step(moons, 1000)
-    Seq(moons.map(_.energy).sum).mkString(" ")
+    var moons = inputs.map(input => newMoon(input))
+    1 to 1000 foreach {_ =>
+      moons = step(moons)
+    }
+    val energy = moons.map(_.energy).sum
+    Seq(energy, 1000).mkString(" ")
   }
 }
